f5ea4d554d6efc92bf1045757f16dc7a9f7d3948
Gustavo V. Diaz <gus838@gmail.com>
2020-09-05 16:13:35 -0300

    Primera versión de ejercicio práctico 2.6

---
+++ Unidad 1/Modulo 2/Scripts/Ejercicio_practico_2_6.sql
@@ -0,0 +1,49 @@
+# Descripción: Ejercicio práctico 2.6
+# Autor: Gustavo Vladimir Diaz
+# Fecha: 05/09/2020
+# Enunciado: 
+# Identifique como dibuja Access las claves primarias en las tablas
+#
+# Objetivo de ejercicio: Mostrar como se identifica la clave primaria en este caso en mysql
+
+# Desarrollo
+
+CREATE DATABASE futbolistas;
+USE futbolistas;
+CREATE TABLE datos_futbolistas (
+    nombre      VARCHAR (30),
+    apellido    VARCHAR(30),
+    edad        INT,
+    posicion    VARCHAR(30),
+    dni         INT,
+    PRIMARY KEY (dni)
+);
+
+INSERT INTO datos_futbolistas (
+    nombre,
+    apellido,
+    edad,
+    posicion,
+    dni)
+    VALUES(
+    "Alberto",
+    "Perez",
+    21,
+    "Delantero",
+    31222333),
+    (
+    "Juan",
+    "Rodriguez",
+    23,
+    "Defensor",
+    30222444),
+    (
+    "José",
+    "Sanchez",
+    25,
+    "Arquero",
+    29444333)
+    ;
+DESCRIBE datos_futbolistas;
+DROP TABLE datos_futbolistas;
+DROP DATABASE futbolistas;

9adef11cb10106b0974db39024a5ed032c0d4843
Gustavo V. Diaz <gdiaz@veng.com.ar>
2020-09-25 19:17:43 -0300

    Create Ejercicio_practico_3_8.sql
    Primera versión de ejercicio 3.8

---
+++ Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_8.sql
@@ -0,0 +1,10 @@
+# Descripción: Ejercicio 3.8
+# Autor: Gustavo Vladimir Diaz
+# Fecha: 25/09/2020
+# Enunciado: 
+# Escribir las sentencias que eliminan las vistas generadas en los ejercicios prácticos 3.4 y 3.5
+#
+# Objetivo de ejercicio: generar vista de un CUIT, IdFactura y el total de la factura
+
+# Desarrollo
+DROP VIEW fact_clientes;

9fb852a009cd6f2cce2887396567407b25432db4
Gustavo V. Diaz <gdiaz@veng.com.ar>
2020-09-25 19:08:24 -0300

    Update Ejercicio_practico_3_6.sql
    Corrección de enunciado en ejercicio 3.6

--- Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_6.sql
+++ Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_6.sql
@@ -2,7 +2,7 @@
 # Autor: Gustavo Vladimir Diaz
 # Fecha: 25/09/2020
 # Enunciado: 
-# Utilizar la vista generada en el ejercicio 3.5
+# Utilizar la vista generada en el ejercicio 3.4 (Lo hago del ejercicio 3.5 porque no existe tal tabla del ejercicio 3.4)
 #
 # Objetivo de ejercicio: generar vista de un CUIT, IdFactura y el total de la factura
 

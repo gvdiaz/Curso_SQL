fb44ae47368521869531ee9f0584d8775c1942c8
Gustavo V. Diaz <gdiaz@veng.com.ar>
2020-09-25 18:27:06 -0300

    Update Ejercicio_practico_3_5.sql
    Terminé de agregar todos los registros necesarios para el ejercicio lpm.

--- Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_5.sql
+++ Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_5.sql
@@ -79,6 +79,50 @@ INSERT INTO facturas(CUIT, IdFactura, Fecha, CondicionDePago, FechaVencimiento, 
     ('20334455660', 8, str_to_date('20,02,2019','%d,%m,%Y'), 'Contado', str_to_date('20,03,2019','%d,%m,%Y'), 3030, 10),
     ('20334455660', 9, str_to_date('22,01,2019','%d,%m,%Y'), 'Tarjeta', str_to_date('22,02,2019','%d,%m,%Y'), 678, 21);
 
+INSERT INTO renglones(IdFactura, IdRenglon, IdArticulo, Cantidad, Precio) VALUES
+	(1, 1, 1, 1, 11),
+    (1, 2, 2, 4, 433),
+    (1, 3, 3, 2, 23),
+    (2, 1, 4, 7, 54),
+    (2, 2, 5, 5, 476),
+    (2, 3, 6, 3, 13),
+    (3, 1, 7, 1, 10),
+    (3, 2, 8, 5, 33),
+    (3, 3, 9, 3, 22),
+    (4, 1, 10, 7, 75),
+    (4, 2, 1, 6, 98),
+    (4, 3, 2, 3, 100),
+    (5, 1, 3, 1, 19),
+    (5, 2, 4, 4, 32),
+    (5, 3, 5, 5, 423),
+    (6, 1, 6, 10, 19),
+    (6, 2, 7, 2, 18),
+    (6, 3, 8, 4, 17),
+    (7, 1, 9, 1, 28),
+    (7, 2, 10, 4, 30),
+    (7, 3, 1, 6, 58),
+    (8, 1, 2, 8, 49),
+    (8, 2, 3, 8, 192),
+    (8, 3, 4, 4, 151),
+    (9, 1, 5, 2, 28),
+    (9, 2, 6, 1, 1),
+    (9, 3, 7, 5, 44),
+    (10, 1, 8,7, 28),
+    (10, 2, 9, 8, 57),
+    (10, 3, 10, 8, 76);
+
+INSERT INTO articulos(IdArticulo, Articulo) VALUES
+	(1, 'Caliza'),
+    (2, 'Mármol'),
+    (3, 'Onix'),
+    (4, 'Granito'),
+    (5, 'Arenisca'),
+    (6, 'Caliza'),
+    (7, 'Pizarra'),
+    (8, 'Basalto'),
+    (9, 'Lunar'),
+    (10, 'Filita');
+
 # Uso de sentencia truncate
 #TRUNCATE TABLE facturas;
 

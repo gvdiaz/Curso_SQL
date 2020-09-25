683243048e7094c97c25dcaf65e26acb0f8c7fea
Gustavo V. Diaz <gdiaz@veng.com.ar>
2020-09-25 19:12:12 -0300

    Create Ejercicio_practico_3_7.sql
    Primera versión ejercicio práctico 3_7

---
+++ Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_7.sql
@@ -0,0 +1,11 @@
+# Descripción: Ejercicio 3.6
+# Autor: Gustavo Vladimir Diaz
+# Fecha: 25/09/2020
+# Enunciado: 
+# Obtener la lista de los cuits de las facturas a partir de la vista generada en el ejercicio práctico 3.5
+#
+# Objetivo de ejercicio: generar vista de un CUIT, IdFactura y el total de la factura
+
+# Desarrollo
+
+SELECT CUIT FROM prueba_cliente_eje_3_5.fact_clientes;

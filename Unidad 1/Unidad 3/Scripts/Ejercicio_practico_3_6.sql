c2c5a2ead86c6030f4bc4e826e25394e19eb31d7
Gustavo V. Diaz <gdiaz@veng.com.ar>
2020-09-25 19:01:34 -0300

    Create Ejercicio_practico_3_6.sql
    Ejecución de vista creada en ejercicio 3.6

---
+++ Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_6.sql
@@ -0,0 +1,11 @@
+# Descripción: Ejercicio 3.6
+# Autor: Gustavo Vladimir Diaz
+# Fecha: 25/09/2020
+# Enunciado: 
+# Utilizar la vista generada en el ejercicio 3.5
+#
+# Objetivo de ejercicio: generar vista de un CUIT, IdFactura y el total de la factura
+
+# Desarrollo
+# Nota: Antes de ejecutar esta sentencia debería ejecutar el script "Ejercicio_practico_3_5.sql" hasta antes de las sentencias DROP.
+SELECT * FROM prueba_cliente_eje_3_5.fact_clientes;

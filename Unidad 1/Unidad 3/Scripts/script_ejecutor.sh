1d121f53b2083b2f586e8e6f4f1ddadde11741ef
Gustavo V. Diaz <gus838@gmail.com>
2020-09-26 16:30:34 -0300

    Agregué comentarios para entender mejor el script, nada más

--- Unidad 1/Unidad 3/Scripts/script_ejecutor.sh
+++ Unidad 1/Unidad 3/Scripts/script_ejecutor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Ejecuta script de mysql que se ingresa como argumento.
+# Ejecuta script de mysql que se ingresa como primer argumento y archivo de salida el segundo argumento (un txt)
 mysql -u "root" -p < $1 > $2

5e2a70b1c6830eede3d01aed2e1b349af938604e
Gustavo V. Diaz <gus838@gmail.com>
2020-09-27 15:19:38 -0300

    Primera versión de ejercicio 3.13

--- Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_13.sql
+++ Unidad 1/Unidad 3/Scripts/Ejercicio_practico_3_13.sql
@@ -13,18 +13,82 @@
 # I. Calcular el logaritmo de 5, el logaritmo de 3 y sumarlos. Luego calcular el logaritmo de 15. ¿Les recuerda alguna propiedad de los logaritmos?
 # J. Calcular el logaritmo de 25 y el logaritmo de 5. ¿Algún recuerdo de lo que pasa con el logaritmo de una potencia?
 #
-# Objetivo de ejercicio: crear un procedimiento que traiga los clientes de determinado año que podremos especificar como entrada en el procedimiento
+# Objetivo de ejercicio: Implementar las funciones pedidas y devolver los resultados.
 
 # Desarrollo
 CREATE DATABASE prueba_eje_3_13;
 USE prueba_eje_3_13;
 
-SELECT 'Raiz cuadrada de 4';
+# Cálculo de raiz cuadrada de 4
+SELECT 'Cálculo de Raiz cuadrada de 4';
 SELECT power(4,1/2);
 
 SELECT '';
 
-SELECT 'Seno 90°';
+# Cálculo de seno de 90°
+SELECT 'Cálculo de Seno 90°';
 SELECT sin(pi()/2);
+
+SELECT '';
+
+# Cálculo de logaritmo natural de 10
+SELECT 'Cálculo de ln(10)';
+SELECT LN(10);
+
+SELECT '';
+
+# Redondear 1/3 a tres decimales
+SELECT 'Redondeo de 1/3 a 3 decimales';
+SELECT ROUND(1/3, 3);
+
+SELECT '';
+
+# Calcular 5 al cubo
+SELECT 'Calcular 5 al cubo';
+SELECT POWER(5,3);
+
+SELECT '';
+
+# Calcular la tangente de 45 grados
+SELECT 'Calcular la tangente de 45 grados';
+SELECT TAN(PI()/4);
+
+SELECT '';
+
+# Calcular el coseno de 180 grados
+SELECT 'Calcular el coseno de 180 grados';
+SELECT COS(PI());
+
+SELECT '';
+
+# Calcular la raíz quinta de 32
+SELECT 'Calcular la raíz quinta de 32';
+SELECT POWER(32,1/5);
+
+SELECT '';
+
+# Calcular el logaritmo de 5, el logaritmo de 3 y sumarlos
+SELECT 'Calcular el logaritmo de 5, el logaritmo de 3 y sumarlos';
+SELECT LOG(5);
+SELECT LOG(3);
+SELECT LOG(5)+LOG(3);
+
+SELECT '';
+
+# Luego calcular el logaritmo de 15
+SELECT 'Luego calcular el logaritmo de 15';
+SELECT LOG(15);
+SELECT LOG(15)-(LOG(5)+LOG(3));
+# La propiedad a la cual hace referencia es la que indica la relación entre el producto log (a) + log(b) = log (a*b)
+
+SELECT '';
+
+# Calcular el logaritmo de 25 y el logaritmo de 5
+SELECT 'Calcular el logaritmo de 25 y el logaritmo de 5';
+SELECT LOG(25);
+SELECT LOG(5);
+SELECT 2*LOG(5) - LOG(25);
+
+SELECT '';
 
 DROP DATABASE prueba_eje_3_13;
